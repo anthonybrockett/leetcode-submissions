ba4e836e9154fde79b57445942385f0bdce51ca5
Tony Brockett <ajbrockett1991@gmail.com>
2022-09-25 11:36:31 -0700

    Time: 208 ms (20.35%), Space: 53 MB (89.42%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -4,9 +4,9 @@
  */
 
 var productExceptSelf = function(nums) {
-    // Array to hold final products
+    // Array to hold final products - fill array same legth as input with 1's (starting at 1 to avoid the first numerical value for products)
     const products = Array(nums.length).fill(1);
-    // Value to increment for each index
+    // Set base value to increase for each product
     let increment = 1;
     // Add items to products from front
     for(let i = 0; i < nums.length; i++) {
