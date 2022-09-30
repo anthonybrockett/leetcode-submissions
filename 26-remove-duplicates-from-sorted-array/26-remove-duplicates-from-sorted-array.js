93e56374dc702368dc7359bc4a6f7d202a61ece2
Tony Brockett <ajbrockett1991@gmail.com>
2022-09-29 19:24:31 -0700

    Time: 117 ms (63.26%), Space: 44.7 MB (49.13%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let idx = 1
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i - 1] !== nums[i]){  
+            nums[idx] = nums[i];  
+            idx++; 
+        }
+    }
+    return idx
+};
