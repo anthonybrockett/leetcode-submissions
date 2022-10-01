8c4c6281a9dc3bdf3ddc93d7fab7d573426fb8b0
Tony Brockett <ajbrockett1991@gmail.com>
2022-10-01 15:39:05 -0700

    Time: 172 ms (52.69%), Space: 48.4 MB (76.88%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    const squares = [];
+    for(let i = 0; i < nums.length; i++) {
+        squares[i] = nums[i] * nums[i]
+    }
+    squares.sort(function(a, b){return a-b})
+    return squares
+};
