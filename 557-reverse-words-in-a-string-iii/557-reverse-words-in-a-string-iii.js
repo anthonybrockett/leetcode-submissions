df18cb06e31cd1860fa2c11c185c7e7c6acd652d
Tony Brockett <ajbrockett1991@gmail.com>
2022-10-06 08:47:08 -0700

    Time: 135 ms (36.87%), Space: 48.1 MB (81.87%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let reversed = [];
+    let split = s.split(' ');
+    // console.log(split)
+    split.forEach(function(t) {
+        t = t.split('').reverse().join('')
+        reversed.push(t)
+    })
+    return reversed.join(' ')
+};
