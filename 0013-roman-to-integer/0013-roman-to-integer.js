ee0dc386f39b35eeb26919ad658e457ffcb2b766
Tony Brockett <ajbrockett1991@gmail.com>
2022-10-12 18:23:47 -0700

    Time: 208 ms (64.08%), Space: 47.1 MB (64.40%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const numerals = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    const len = s.length;
+    let total = 0;
+
+    for (let i = 0; i < len; i++) {
+        if (i < len - 1 && numerals[s[i + 1]] > numerals[s[i]]) {
+            total -= numerals[s[i]];
+        } else {
+            total += numerals[s[i]];
+        }
+    }
+    return total;
+};
